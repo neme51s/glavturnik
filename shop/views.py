60c26fe56f2ff022d44b61d4ccfc47b18c878c2d
neme51s <paradox28022006@gmail.com>
2026-05-25 16:48:13 +0600

    Fix views: remove order field from order_by (field deleted in migration)

--- shop/views.py
+++ shop/views.py
@@ -5,7 +5,7 @@ from .models import Category, Product
 def index(request):
     categories = Category.objects.prefetch_related('products').all()
     hits = Product.objects.filter(is_hit=True).select_related('category')
-    all_products = Product.objects.select_related('category').order_by('-is_hit', 'order')
+    all_products = Product.objects.select_related('category').order_by('-is_hit', 'name')
     return render(request, 'shop/index.html', {
         'categories': categories,
         'hits': hits,
